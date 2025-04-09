cd1f91d262611b1603536dd14299d75a141522d0
Leonardo Palma <leonardoc.palma@outlook.com>
2025-04-08 22:29:35 -0300

    feat: CRUD party finished

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -6,4 +6,10 @@ const serviceRouter = require("./services");
 
 router.use("/", serviceRouter);
 
+// Parties routes
+
+const partyRouter = require ("./parties");
+
+router.use("/", partyRouter);
+
 module.exports = router;
