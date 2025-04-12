aa768e79068851704f435c2ca014c782647ffa7a
Leonardo Palma <leonardoc.palma@outlook.com>
2025-04-12 00:04:51 -0300

    feat:Created home and css, applications database services

--- frontend/src/routes/CreateParty.jsx
+++ frontend/src/routes/CreateParty.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
 
 const CreateParty = () => {
   return (
-    <div>CreateParty</div>
-  )
-}
+    <div className="form-page">
+      <h2>Crie sua próxima festa</h2>
+      <p>Defina o seu orçamento e escolha os serviços</p>
+      <form>
+        <label>
+          <span>Nome da festa:</span>
+          <input type="text" placeholder="Seja Criativo..." required />
+        </label>
+        <label>
+          <span>Anfitrião:</span>
+          <input
+            type="text"
+            placeholder="Quem está fazendo a festa?"
+            required
+          />
+        </label>
+        <label>
+          <span>Descrição:</span>
+          <textarea placeholder="Descreva o seu evento..." required></textarea>
+        </label>
+        <label>
+          <span>Orçamento:</span>
+          <input
+            type="number"
+            placeholder="Quanto você pretende investir?"
+            required
+          />
+        </label>
+        <label htmlFor="">
+          <span>Imagem:</span>
+          <input type="text" placeholder="Insira a URL da imagem" required />
+        </label>
+        <div>
+          <h2>Escolha os serviços</h2>
+          <div className="services-container">
+            <p>Serviços...</p>
+          </div>
+        </div>
+        <input type="submit" value={"Criar Festa"} />
+      </form>
+    </div>
+  );
+};
 
-export default CreateParty
+export default CreateParty;
--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import "./Home.css";
+
+import React from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import partyFetch from "../axios/config";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [parties, setParties] = useState(null);
 
-export default Home
+  // load parties
+  useEffect(() => {
+    const loadParties = async () => {
+      const res = await partyFetch.get("/parties");
+
+      console.log(res);
+
+      setParties(res.data);
+    };
+
+    loadParties();
+  }, []);
+
+  if (!parties) return <p>Carregando...</p>;
+
+  return (
+    <div className="home">
+      <h1>Suas Festas</h1>
+      <div className="parties-container">
+        {parties.length === 0 && <p>Voce ainda nao criou nenhuma festa</p>}
+        {parties.map((party) => (
+          <div className="party" key={party._id}>
+            <img src={party.image} alt={party.title} />
+            <h3>{party.title}</h3>
+            <Link to={`/party/${party._id}`} className="btn-secondary">
+              Detalhes
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
