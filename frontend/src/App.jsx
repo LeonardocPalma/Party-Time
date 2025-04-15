8d1bb75a3172016f21fe22f06db93a4886a97e45
Leonardo Palma <leonardoc.palma@outlook.com>
2025-04-14 21:23:04 -0300

    feat:created page party and function button details and button delete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
 
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 //
 import Navbar from "./components/Navbar";
@@ -8,6 +10,7 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
+      <ToastContainer />
       <Navbar />
       <Outlet />
     </div>
--- frontend/src/routes/CreateParty.jsx
+++ frontend/src/routes/CreateParty.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import "./Form.css";
 
 import React from "react";
@@ -5,9 +6,19 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import partyFetch from "../axios/config";
+import useToast from "../hook/useToast";
 
 const CreateParty = () => {
   const [services, setServices] = useState([]);
+
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [description, setDescription] = useState("");
+  const [budget, setBudget] = useState(0);
+  const [image, setImage] = useState("");
+  const [partyServices, setPartyServices] = useState([]);
+
+  const navigate = useNavigate();
 
   // load services
   useEffect(() => {
@@ -20,14 +31,59 @@ const CreateParty = () => {
     loadServices();
   }, []);
 
+  // add or remove services
+  const handleServices = (e) => {
+    const checked = e.target.checked;
+    const value = e.target.value;
+
+    const filteredServices = services.filter((s) => s._id === value);
+
+    if (checked) {
+      setPartyServices((services) => [...services, filteredServices[0]]);
+    } else {
+      setPartyServices((services) => services.filter((s) => s._id !== value));
+    }
+  };
+
+  const createParty = async (e) => {
+    e.preventDefault();
+
+    try {
+      const party = {
+        title,
+        author,
+        description,
+        budget,
+        image,
+        services: partyServices,
+      };
+
+      const res = await partyFetch.post("/parties", party);
+
+      if (res.status === 201) {
+        navigate("/");
+
+        useToast(res.data.msg);
+      }
+    } catch (error) {
+      useToast(error.response.data.msg, "error");
+    }
+  };
+
   return (
     <div className="form-page">
       <h2>Crie sua próxima festa</h2>
       <p>Defina o seu orçamento e escolha os serviços</p>
-      <form>
+      <form onSubmit={(e) => createParty(e)}>
         <label>
           <span>Nome da festa:</span>
-          <input type="text" placeholder="Seja Criativo..." required />
+          <input
+            type="text"
+            placeholder="Seja Criativo..."
+            required
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
         </label>
         <label>
           <span>Anfitrião:</span>
@@ -35,11 +91,18 @@ const CreateParty = () => {
             type="text"
             placeholder="Quem está fazendo a festa?"
             required
+            onChange={(e) => setAuthor(e.target.value)}
+            value={author}
           />
         </label>
         <label>
           <span>Descrição:</span>
-          <textarea placeholder="Descreva o seu evento..." required></textarea>
+          <textarea
+            placeholder="Descreva o seu evento..."
+            required
+            onChange={(e) => setDescription(e.target.value)}
+            value={description}
+          ></textarea>
         </label>
         <label>
           <span>Orçamento:</span>
@@ -47,11 +110,19 @@ const CreateParty = () => {
             type="number"
             placeholder="Quanto você pretende investir?"
             required
+            onChange={(e) => setBudget(e.target.value)}
+            value={budget}
           />
         </label>
         <label htmlFor="">
           <span>Imagem:</span>
-          <input type="text" placeholder="Insira a URL da imagem" required />
+          <input
+            type="text"
+            placeholder="Insira a URL da imagem"
+            required
+            onChange={(e) => setImage(e.target.value)}
+            value={image}
+          />
         </label>
         <div>
           <h2>Escolha os serviços</h2>
@@ -64,14 +135,18 @@ const CreateParty = () => {
                   <p className="service-name">{service.name}</p>
                   <p className="service-price">R$ {service.price}</p>
                   <div className="checkbox-container">
-                    <input type="checkbox" value={service._id} />
+                    <input
+                      type="checkbox"
+                      value={service._id}
+                      onChange={(e) => handleServices(e)}
+                    />
                     <p>Selecionar</p>
                   </div>
                 </div>
               ))}
           </div>
         </div>
-        <input type="submit" value={"Criar Festa"} className="btn"/>
+        <input type="submit" value={"Criar Festa"} className="btn" />
       </form>
     </div>
   );
